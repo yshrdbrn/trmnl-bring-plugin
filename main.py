e3fcfd34c7a8832d4d05cd2cc4fb63ad8ae479ab
Yashar Dabiran <yashardabiran75@gmail.com>
2025-02-12 01:04:27 -0500

    Add Dockerfile and fix deprecated call

--- main.py
+++ main.py
@@ -52,7 +52,8 @@ class BringPlugin:
 
 if __name__ == "__main__":
     bring_plugin = BringPlugin()
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     
     while True:
         try:
